8ccdf205f1fdbf45dd0fe46e757a7c118f935204
Dragon987 <dragolekovic@gmail.com>
2022-04-29 19:23:21 +0200

    Add project files.

---
+++ EDnevnik/Osoba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EDnevnik
+{
+    public partial class Osoba : Form
+    {
+        public Osoba()
+        {
+            InitializeComponent();
+        }
+    }
+}
